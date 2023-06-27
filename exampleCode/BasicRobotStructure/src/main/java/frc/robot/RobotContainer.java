ece617cebd8da3ef1cd4de04293225fe6480e194
Martin Dionne <>
2023-06-27 19:39:41 -0400

    Rename m_controller to m_gamepad

--- exampleCode/BasicRobotStructure/src/main/java/frc/robot/RobotContainer.java
+++ exampleCode/BasicRobotStructure/src/main/java/frc/robot/RobotContainer.java
@@ -31,7 +31,7 @@ public class RobotContainer {
   private final OnBoardIO m_onboardIO = new OnBoardIO(ChannelMode.INPUT, ChannelMode.INPUT);
 
   // Assumes a gamepad plugged into channnel 0
-  private final Joystick m_controller = new Joystick(0);
+  private final Joystick m_gamepad = new Joystick(0);
 
   // Create SmartDashboard chooser for autonomous routines
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -92,6 +92,6 @@ public class RobotContainer {
    */
   public Command getArcadeDriveCommand() {
     return new ArcadeDrive(
-        m_drivetrain, () -> -m_controller.getRawAxis(1), () -> -m_controller.getRawAxis(4));
+        m_drivetrain, () -> -m_gamepad.getRawAxis(1), () -> -m_gamepad.getRawAxis(4));
   }
 }
